8ed429f9fae6017bc8971ebb6bf34958bbda7a00
GlitchyZorua <54043124+GlitchyZorua@users.noreply.github.com>
2024-05-02 15:07:12 -0700

    Update 8ball.js

--- Win9Arc/Scripts/Apps/GlitchyZorua/The magic 8ball/8ball.js
+++ Win9Arc/Scripts/Apps/GlitchyZorua/The magic 8ball/8ball.js
@@ -1,4 +1,4 @@
-// created by Jacobw#8117
+// created by GlitchyZorua
 // v0.0.0
 $prompt('Question?', '', function(ok) {
     var ballanswer = ['It is certain.','It is decidedly so.','Without a doubt.','Yes definitely.','You may rely on it.','Yes.','As I see it, yes.','Most likely','Outlook Good.','Signs point to yes.','Reply hazy, try again','Ask again later','Better not tell you now.','Cannot predict now.','Concentrate and ask again.','Don\'t count on it.','No.','My reply is no.','Outlook not so good.','Maybe.','Very doubtful.']
