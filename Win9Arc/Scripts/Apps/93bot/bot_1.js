daee249feeb2e3ad39235883fc32b32353bfead2
GlitchyZorua <54043124+GlitchyZorua@users.noreply.github.com>
2024-05-25 20:24:22 -0700

    Update and rename bot_1.txt to bot_1.js

---
+++ Win9Arc/Scripts/Apps/93bot/bot_1.js
@@ -0,0 +1,142 @@
+﻿var quotes = ['http://generated.inspirobot.me/082/aXm7345xjU.jpg','http://generated.inspirobot.me/082/aXm3437xjU.jpg','http://generated.inspirobot.me/083/aXm3704xjU.jpg','http://generated.inspirobot.me/084/aXm5668xjU.jpg','http://generated.inspirobot.me/086/aXm5258xjU.jpg','http://generated.inspirobot.me/083/aXm4342xjU.jpg','http://generated.inspirobot.me/086/aXm3614xjU.jpg','http://generated.inspirobot.me/082/aXm8948xjU.jpg','http://generated.inspirobot.me/082/aXm8466xjU.jpg','http://generated.inspirobot.me/086/aXm7608xjU.jpg','http://generated.inspirobot.me/085/aXm9191xjU.jpg','http://generated.inspirobot.me/082/aXm7315xjU.jpg','http://generated.inspirobot.me/084/aXm777xjU.jpg','http://generated.inspirobot.me/083/aXm8469xjU.jpg ','http://generated.inspirobot.me/085/aXm1138xjU.jpg','http://generated.inspirobot.me/083/aXm609xjU.jpg','http://generated.inspirobot.me/084/aXm8616xjU.jpg','http://generated.inspirobot.me/082/aXm7407xjU.jpg','http://generated.inspirobot.me/082/aXm4866xjU.jpg','http://generated.inspirobot.me/084/aXm9385xjU.jpg','http://generated.inspirobot.me/084/aXm616xjU.jpg','http://generated.inspirobot.me/082/aXm2494xjU.jpg','http://generated.inspirobot.me/084/aXm3446xjU.jpg','http://generated.inspirobot.me/082/aXm1953xjU.jpg','http://generated.inspirobot.me/086/aXm3823xjU.jpg','http://generated.inspirobot.me/086/aXm2976xjU.jpg','http://generated.inspirobot.me/084/aXm5790xjU.jpg','http://generated.inspirobot.me/082/aXm9536xjU.jpg','http://generated.inspirobot.me/085/aXm8797xjU.jpg','http://generated.inspirobot.me/083/aXm3915xjU.jpg','http://generated.inspirobot.me/084/aXm7948xjU.jpg','http://generated.inspirobot.me/084/aXm4712xjU.jpg','http://generated.inspirobot.me/083/aXm7339xjU.jpg','http://generated.inspirobot.me/085/aXm4343xjU.jpg','http://generated.inspirobot.me/082/aXm4378xjU.jpg','http://generated.inspirobot.me/086/aXm9375xjU.jpg','http://generated.inspirobot.me/083/aXm8764xjU.jpg','http://generated.inspirobot.me/084/aXm4352xjU.jpg','http://generated.inspirobot.me/082/aXm9301xjU.jpg','http://generated.inspirobot.me/086/aXm5607xjU.jpg','http://generated.inspirobot.me/085/aXm7718xjU.jpg','http://generated.inspirobot.me/083/aXm4064xjU.jpg','http://generated.inspirobot.me/085/aXm475xjU.jpg','http://generated.inspirobot.me/082/aXm8025xjU.jpg','http://generated.inspirobot.me/085/aXm9747xjU.jpg','http://generated.inspirobot.me/083/aXm7781xjU.jpg','http://generated.inspirobot.me/083/aXm8086xjU.jpg','http://generated.inspirobot.me/083/aXm146xjU.jpg','http://generated.inspirobot.me/084/aXm1748xjU.jpg','http://generated.inspirobot.me/086/aXm2124xjU.jpg','http://generated.inspirobot.me/082/aXm3391xjU.jpg','http://generated.inspirobot.me/085/aXm750xjU.jpg','http://generated.inspirobot.me/082/aXm7586xjU.jpg','http://generated.inspirobot.me/083/aXm4917xjU.jpg','http://generated.inspirobot.me/083/aXm4478xjU.jpg','http://generated.inspirobot.me/082/aXm432xjU.jpg','http://generated.inspirobot.me/082/aXm310xjU.jpg','http://generated.inspirobot.me/082/aXm3204xjU.jpg','http://generated.inspirobot.me/085/aXm9885xjU.jpg','http://generated.inspirobot.me/086/aXm1861xjU.jpg','http://generated.inspirobot.me/084/aXm2071xjU.jpg','http://generated.inspirobot.me/083/aXm381xjU.jpg','http://generated.inspirobot.me/084/aXm7015xjU.jpg','http://generated.inspirobot.me/084/aXm4260xjU.jpg','http://generated.inspirobot.me/083/aXm7656xjU.jpg','http://generated.inspirobot.me/084/aXm4358xjU.jpg','http://generated.inspirobot.me/084/aXm3466xjU.jpg','http://generated.inspirobot.me/084/aXm9606xjU.jpg','http://generated.inspirobot.me/082/aXm8334xjU.jpg','http://generated.inspirobot.me/084/aXm6201xjU.jpg','http://generated.inspirobot.me/085/aXm6812xjU.jpg','http://generated.inspirobot.me/082/aXm7582xjU.jpg','http://generated.inspirobot.me/082/aXm9099xjU.jpg','http://generated.inspirobot.me/082/aXm1668xjU.jpg','http://generated.inspirobot.me/086/aXm7839xjU.jpg','http://generated.inspirobot.me/083/aXm4292xjU.jpg','http://generated.inspirobot.me/085/aXm1521xjU.jpg','http://generated.inspirobot.me/085/aXm7627xjU.jpg','http://generated.inspirobot.me/083/aXm7929xjU.jpg','http://generated.inspirobot.me/086/aXm923xjU.jpg','http://generated.inspirobot.me/086/aXm2513xjU.jpg','http://generated.inspirobot.me/084/aXm7383xjU.jpg','http://generated.inspirobot.me/086/aXm2813xjU.jpg','http://generated.inspirobot.me/084/aXm6831xjU.jpg','http://generated.inspirobot.me/083/aXm2065xjU.jpg'] //less quotes
+var trollboxMessagesSent = 0
+var trollboxMessagesSentThisMinute = 0;
+var trollboxMessagesSentMinute = []
+var ball = ['Yes.','No.','I think so.','Maybe?','Ask again later.','Pretty sure.','Not at all.','Probably.','Probably not.','I think so?','Not that I remember.']
+var leaves = 0
+var joins = 0
+var LMoS = 0
+var TS = 0
+setInterval(function(){
+	TS += 0.1
+},100)
+function getAvarage(array){
+      var e = 0;
+    for(let i = 0;i < array.length;i++){
+        e = e + array[i]
+    }
+      return Math.round(e / array.length)
+}
+setInterval(function(){
+    trollboxMessagesSentMinute.push(trollboxMessagesSentThisMinute)
+    trollboxMessagesSentThisMinute = 0
+},60000)
+var thisDate;
+socket.on('message',function(data){
+	if(TS > LMoS){
+		LMoS = TS
+	}
+	TS = 0
+	trollboxMessagesSent++
+    	trollboxMessagesSentThisMinute++
+	thisDate = new Date();
+  	data.msg = decodeHtmlEntity(data.msg)
+    data.nick = decodeHtmlEntity(data.nick)
+    data.msg = data.msg.trim()
+  	if(data.msg.startsWith('!bot ')&&data.nick!=undefined){
+    	data.msg = data.msg.slice(5);
+      	if(data.msg.startsWith('date')){
+        	socket.emit('message','📆 '+thisDate.getMonth()+'/'+thisDate.getDate()+'/'+thisDate.getFullYear())
+        }
+        if(data.msg.startsWith('time')){
+        	socket.emit('message','🕒 '+thisDate.getHours()+':'+thisDate.getMinutes()+' (24 Hour Time)')
+        }
+      	if(data.msg.startsWith('help')){
+        	socket.emit('message','❔ help, date, time, rename <name>, echo <text>, dice [number of sides], ae <js>, color <hex/Color name>, joins, leaves, msgSent, MpM, MtM, 8ball [A qustion], LMoS, hi, hello, Trophy\nDiscord: https://discord.gg/Ms4NFJb')
+        }
+	if(data.msg.startsWith('8ball')){
+        	socket.emit('message','🎱 '+ball[Math.floor(Math.random()*ball.length)])
+        }
+	if(data.msg.startsWith('LMoS')){
+        	socket.emit('message','🔇 '+(Math.round(LMoS*10)/10))
+        }
+      	if(data.msg.startsWith('ae ')){
+        	data.msg = data.msg.slice(3)
+            if(data.color.split(';')[1] == 'lol'){
+            	sendMsg(eval(data.msg))
+            }else{
+            	sendMsg('❌ No admin permissions.')
+            }
+        }
+	if(data.msg.startsWith('quote')){
+          	var e = Math.floor(Math.random()*quotes.length)
+            	sendMsg(quotes[e])
+        }
+      	if(data.msg.startsWith('color ')){
+        	data.msg = data.msg.slice(6)
+            socket.emit('user joined', pseudo, data.msg, style, pass);
+        }
+      	if(data.msg.startsWith('rename ')){
+        	data.msg = data.msg.slice(7)
+            if(data.msg == pseudo){
+            	socket.emit('message','❌ That\'s already my name')
+            }else{
+            	setPseudo(data.msg)
+            }
+          	return;
+        }
+      	if(data.msg.startsWith('dice')){
+      		var e = data.msg.slice(5)
+            e = e || 6
+            e = Number(e)
+            if((e+'').match(/\./)||e < 2){
+            	sendMsg('⚠️ No. Just no.')
+            }
+            e = Math.floor(Math.random() * e) + 1;  
+          	sendMsg('🎲 '+e)
+        }
+      	if(data.msg.startsWith('echo ')){
+        	data.msg = data.msg.slice(5) 
+            if(data.msg.startsWith('/color ')){
+            	socket.emit('message','⚠️ Use the color command.')
+                return;
+            }
+          	if(data.msg.length > 1000){
+            	socket.emit('message','⚠️ There is a maximum of 1,000 charaters.')
+                return;
+            }
+          	if(data.msg.startsWith('/sin')){
+            	socket.emit('message','❌ no')
+                return;
+            }
+          	if(data.msg == ('/block '+pseudo)){
+            	socket.emit('message','❌ Trying to mess with the Bot files I see....\nNICE TRY.')
+                return;
+            }
+          	socket.emit('message','💬 '+data.msg+' ~~'+data.nick)
+        }
+        	if(data.msg.startsWith('msgSent')){
+            sendMsg('💬 '+trollboxMessagesSent)
+        }
+         	if(data.msg.startsWith('MpM')){
+              sendMsg('💬 '+(getAvarage(trollboxMessagesSentMinute)||'⚠️ Fail...'))
+        }
+         	if(data.msg.startsWith('MtM')){
+              sendMsg('💬 '+trollboxMessagesSentThisMinute)
+        }
+         	if(data.msg.startsWith('MtM')){
+              sendMsg('💬 '+trollboxMessagesSentThisMinute)
+        }
+        	if(data.msg.startsWith('joins')){
+              sendMsg('➕ '+joins)
+        }
+        	if(data.msg.startsWith('leaves')){
+              sendMsg('➖ '+leaves)
+        }
+        	if(data.msg.startsWith('hi')){
+              sendMsg('👋 Why hello there!')
+        }
+        	if(data.msg.startsWith('hello')){
+              sendMsg('👋 Why hello there!')
+        }
+        	if(data.msg.startsWith('Trophy')){
+              sendMsg("🏆 Welcome to teh Trophy room!\n1. Welcome Trophy")
+        }
+    };
+});
+socket.on('user left',function(){
+    leaves++
+})
+socket.on('user joined',function(){
+    joins++
+})
